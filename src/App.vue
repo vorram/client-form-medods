771052da50ff0ca1de58fd5592b4f74fc6e58861
Andrey Tsygul <vorram1@gmail.com>
2022-02-09 03:15:35 +0300

    Добавлено сообщение при успешном заполнении формы

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-    <ClientForm />
+    <ClientForm v-show="!this.formSubmitted" v-on:form-submit="this.toggleDisplay" />
+    <SubmitMessage v-show="this.formSubmitted" v-on:reset-form="this.toggleDisplay" />
   </div>
 </template>
 
 <script>
 import ClientForm from './components/ClientForm.vue'
+import SubmitMessage from './components/SubmitMessage.vue'
 
 export default {
   name: 'App',
   components: {
     ClientForm,
+    SubmitMessage,
+  },
+  data() {
+    return {
+      formSubmitted: false,
+    }
+  },
+  methods: {
+    toggleDisplay() {
+      if (this.formSubmitted) {
+        this.formSubmitted = false
+      } else {
+        this.formSubmitted = true
+      }
+    },
   },
 }
 </script>
